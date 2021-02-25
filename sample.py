b2d63644b3f5590c31dfac46d9139ee140f09507
Kevin Li <likevinsen@gmail.com>
2021-02-25 15:51:01 -0600

    adding more data sets

--- sample.py
+++ sample.py
@@ -7,8 +7,8 @@ from constructor.pedigree import construct_graph
 from constructor.util import parse_data, visualize_graph
 from os import path, remove
 
-INPUT_BIO = 'siblings_bio.csv'
-INPUT_DEGREE = 'siblings_degrees.csv'
+INPUT_BIO = 'disjoint_bio.csv'
+INPUT_DEGREE = 'disjoint_degrees.csv'
 
 def main():
 
